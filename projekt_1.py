299a5b9eb38986cefacd6d9e83400fad641b4f85
01160220@pw.edu.pl <01160220@pw.edu.pl>
2022-04-12 21:28:34 +0200

    Dodano funkcje

--- projekt_1.py
+++ projekt_1.py
@@ -18,3 +18,38 @@ class Transformacje:
         
         self.flattening = (self.a-self.b)/ self.a
         self.ecc = 2*self.flattening - self.flattening**2
+
+    def xyz_2_blh(X, Y, Z, self):
+        """
+        Funkcja przelicza współrzędne geocentryczne (ECEF) 
+        na współrzędne geodezyjne (Algorytm Hirvonena).
+
+        Parameters
+        ----------
+        X   : [float] : współrzędna geocentryczna (ECEF) [m]
+        Y   : [float] : współrzędna geocentryczna (ECEF) [m]
+        Z   : [float] : współrzędna geocentryczna (ECEF) [m]
+        a   : [float] : dłuższa półoś elipsoidy [m]
+        e2  : [float] : mimośrod elipsoidy [niemianowana]
+        Returns
+        -------
+        fi  : [float] : szerokość geodezyjna [rad]
+        lam : [float] : długość geodezyjna [rad]
+        h   : [float] : wysokość elipsoidalna [m]
+
+        """
+        r = np.sqrt(X**2+Y**2)
+        fi = np.arctan(Z/(r*(1-self.ecc)))
+        eps = 0.000001/3600*m.pi/180
+        fi0 = fi*2
+
+        while abs((fi - fi0).all()) > eps:
+            fi0 = fi
+            N = self.a/np.sqrt(1-self.ecc*np.sin(fi)**2)
+            h = r/np.cos(fi)-N
+            fi = np.arctan(Z/(r*(1-self.ecc*(N/(N+h)))))
+
+        lam = np.arctan(Y/X)
+        N = self.a/np.sqrt(1-self.ecc*np.sin(fi)**2)
+        h = r/np.cos(fi)-N
+        return fi, lam, h 
